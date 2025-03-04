0294ef421b3d47e2471811f8a7c174e4b961b792
tcmofashi <mofashiforzbx@qq.com>
2025-03-04 11:33:10 +0800

    fix: 增加print

--- src/plugins/chat/config.py
+++ src/plugins/chat/config.py
@@ -81,8 +81,10 @@ class BotConfig:
                 personality_config=toml_dict['personality']
                 personality=personality_config.get('prompt_personality')
                 if len(personality) >= 2:
-                    config.PROMPT_PERSONALITY=personality_config.get('prompt_personality')
-                config.PROMPT_SCHEDULE_GEN=personality_config.get('prompt_schedule')
+                    print(f"载入自定义人格:{personality}")
+                    config.PROMPT_PERSONALITY=personality_config.get('prompt_personality',config.PROMPT_PERSONALITY)
+                print(f"载入自定义日程prompt:{personality_config.get('prompt_schedule',config.PROMPT_SCHEDULE_GEN)}")
+                config.PROMPT_SCHEDULE_GEN=personality_config.get('prompt_schedule',config.PROMPT_SCHEDULE_GEN)
 
             if "emoji" in toml_dict:
                 emoji_config = toml_dict["emoji"]
