cfcdcc653ece95a31595bbd9a6882c4473edb1ab
Rikki <rikki@member.fsf.org>
2025-03-07 18:01:32 +0800

    update: 更新开发必要的包，但是因为 nb-cli 上游打包中并未包含 nonebot2，因此目前本配置并不能用于运行和调试

--- flake.nix
+++ flake.nix
@@ -1,50 +1,59 @@
 {
   description = "MaiMBot Nix Dev Env";
+  # 本配置仅方便用于开发，但是因为 nb-cli 上游打包中并未包含 nonebot2，因此目前本配置并不能用于运行和调试
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
         };
 
-        # 读取 requirements.txt 文件
-        requirementsFile = builtins.readFile ./requirements.txt;
-
-        # 解析 requirements.txt 文件，提取包名
-        parseRequirements = content:
-          let
-            lines = builtins.split "\n" content;
-            # 过滤掉空行和注释
-            filteredLines = builtins.filter (line:
-              line != "" && !(builtins.match "^ *#.*" line)
-            ) lines;
-            # 提取包名（去掉版本号）
-            packageNames = builtins.map (line:
-              builtins.head (builtins.split "[=<>]" line)
-            ) filteredLines;
-          in
-          packageNames;
-
-        # 获取 requirements.txt 中的包名列表
-        requirements = parseRequirements requirementsFile;
-
-        # 动态生成 Python 环境
-        pythonEnv = pkgs.python3.withPackages (ps:
-          builtins.map (pkg: ps.${pkg}) requirements
+        pythonEnv = pkgs.python3.withPackages (
+          ps: with ps; [
+            pymongo
+            python-dotenv
+            pydantic
+            jieba
+            openai
+            aiohttp
+            requests
+            urllib3
+            numpy
+            pandas
+            matplotlib
+            networkx
+            python-dateutil
+            APScheduler
+            loguru
+            tomli
+            customtkinter
+            colorama
+            pypinyin
+            pillow
+            setuptools
+          ]
         );
       in
       {
         devShell = pkgs.mkShell {
-          buildInputs = [ pythonEnv ];
+          buildInputs = [
+            pythonEnv
+            pkgs.nb-cli
+          ];
 
           shellHook = ''
-            echo "Python environment is ready!"
           '';
         };
       }
