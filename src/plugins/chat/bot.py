985b8728288e17d4ed1e50abadeff8417480a352
tcmofashi <mofashiforzbx@qq.com>
2025-03-29 14:46:50 +0800

    fix: 百分百回复off

--- src/plugins/chat/bot.py
+++ src/plugins/chat/bot.py
@@ -148,7 +148,7 @@ class ChatBot:
 
         response = None
         # 开始组织语言
-        if random() < reply_probability + 1:
+        if random() < reply_probability:
             bot_user_info = UserInfo(
                 user_id=global_config.BOT_QQ,
                 user_nickname=global_config.BOT_NICKNAME,
