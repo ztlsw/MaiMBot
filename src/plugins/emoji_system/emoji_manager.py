a206af76fb8e07d29837df44910cbd8eb3d8f9b8
SengokuCola <1026294844@qq.com>
2025-04-30 18:30:27 +0800

    Update emoji_manager.py

--- src/plugins/emoji_system/emoji_manager.py
+++ src/plugins/emoji_system/emoji_manager.py
@@ -131,7 +131,9 @@ class MaiEmoji:
 
                 # 使用upsert确保记录存在或被更新
                 db["emoji"].update_one({"hash": self.hash}, {"$set": emoji_record}, upsert=True)
-                logger.success(f"[注册] 表情包信息保存到数据库: {self.description[:30]}...")
+                
+                
+                logger.success(f"[注册] 表情包信息保存到数据库: {self.emotion}")
 
                 return True
 
